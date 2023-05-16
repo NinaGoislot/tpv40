192c954751a0b8639db7901a60dcd45b715a99b6
Julie357 <julie.barbouxbitauld@gmail.com>
2023-05-16 18:13:03 +0200

    page article

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+    public function show(Article $article)
+    {
+        return $this->render('article.html.twig', [
+            'article' => $article,
+        ]);
+    }
+}
