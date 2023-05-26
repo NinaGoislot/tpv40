2f02f823489547d040be182cc3b56cfc721e85b1
Julie357 <julie.barbouxbitauld@gmail.com>
2023-05-26 22:35:50 +0200

    Modification page article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,8 +11,15 @@ class ArticleController extends AbstractController
     /**
      * @Route("/article", name="article_show")
      */
-    public function show(Article $article)
+    public function showArticle($id)
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = $entityManager->getRepository(Article::class)->find($id);
+
+        if (!$article) {
+            throw $this->createNotFoundException('Article not found');
+        }
+
         return $this->render('article.html.twig', [
             'article' => $article,
         ]);
